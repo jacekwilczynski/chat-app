1f0bb0c9ae1dd908120b84013635b5430b4c42c0
Jacek Wilczyński <jacekwilczynski5@gmail.com>
2018-08-05 19:45:00 +0200

    Refactor

--- src/middlewares/throttle.js
+++ src/middlewares/throttle.js
@@ -1,21 +1,4 @@
-const Register = () => {
-  const state = {};
-  return {
-    isThrottled: actionType =>
-      state[actionType] && state[actionType].isThrottled,
-    throttle: actionType => {
-      state[actionType] = { isThrottled: true };
-    },
-    tryLater: (actionType, callback) => {
-      state[actionType].onDethrottle = callback;
-    },
-    dethrottle: actionType => {
-      const retry = state[actionType].onDethrottle;
-      state[actionType] = null;
-      if (retry) retry();
-    }
-  };
-};
+import Register from 'middlewares/throttle/Register';
 
 const actions = Register();
 
