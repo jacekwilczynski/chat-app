3fd8ab6ded55ec6603344673a77ef180215c68de
Jacek Wilczyński <jacekwilczynski5@gmail.com>
2018-08-10 07:19:41 +0200

    Formalize nickname messages

--- src/client/middlewares/nickname.js
+++ src/client/middlewares/nickname.js
@@ -1,16 +1,12 @@
 import * as nicknameActions from 'client/actions/nickname';
 import * as socketActions from 'client/actions/socket';
+import * as nicknameMessages from 'common/messages/nickname';
 
 const checkAvailability = ({ dispatch }) => next => action => {
   next(action);
   if (action.type === nicknameActions.CHECK_AVAILABILITY) {
     const nickname = action.payload;
-    dispatch(
-      socketActions.send({
-        type: '[nickname] CHECK_AVAILABILITY',
-        payload: nickname
-      })
-    );
+    dispatch(socketActions.send(nicknameMessages.checkAvailability(nickname)));
   }
 };
 
@@ -18,7 +14,7 @@ const receiveAvailability = ({ dispatch }) => next => action => {
   next(action);
   if (action.type === socketActions.MESSAGE) {
     const message = action.payload;
-    if ((message.type = '[nickname] AVAILABILITY')) {
+    if ((message.type = nicknameMessages.AVAILABILITY)) {
       const { nickname, isAvailable } = message.payload;
       dispatch(
         nicknameActions.updateAvailability({
--- src/server/middlewares/nickname.js
+++ src/server/middlewares/nickname.js
@@ -1,3 +1,4 @@
+import * as nicknameMessages from 'common/messages/nickname';
 import * as serverActions from 'server/actions/server';
 import { isNicknameAvailable } from 'server/reducer';
 
@@ -5,18 +6,15 @@ const check = ({ dispatch, getState }) => next => action => {
   next(action);
   if (action.type === serverActions.MESSAGE) {
     const message = action.payload;
-    if (message.type === '[nickname] CHECK_AVAILABILITY') {
+    if (message.type === nicknameMessages.CHECK_AVAILABILITY) {
       const { socket } = action.meta;
       const nickname = message.payload;
       const isAvailable = isNicknameAvailable(getState(), nickname);
       dispatch(
-        serverActions.send([socket], {
-          type: '[nickname] AVAILABILITY',
-          payload: {
-            isAvailable,
-            nickname
-          }
-        })
+        serverActions.send(
+          [socket],
+          nicknameMessages.availability(nickname, isAvailable)
+        )
       );
     }
   }
