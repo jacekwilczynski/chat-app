56462af54f84e8249a4edaeeebb85865e28938fa
Jacek Wilczyński <jacekwilczynski5@gmail.com>
2018-08-10 07:19:14 +0200

    Remove unused throttle middleware

--- src/client/middlewares.js
+++ src/client/middlewares.js
@@ -1,7 +1,6 @@
 import loginView from 'client/middlewares/loginView';
 import nickname from 'client/middlewares/nickname';
 import socket from 'client/middlewares/socket';
-import throttle from 'client/middlewares/throttle';
 import user from 'client/middlewares/user';
 
-export default [...throttle, ...socket, ...nickname, ...user, ...loginView];
+export default [...socket, ...nickname, ...user, ...loginView];
