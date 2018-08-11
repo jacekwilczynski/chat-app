f1401dad8878ae20ba380e8847a47d16d162e1ac
Jacek Wilczyński <jacekwilczynski5@gmail.com>
2018-08-11 11:46:23 +0200

    Add custom log messages for common server actions

--- src/server/middlewares/logger.js
+++ src/server/middlewares/logger.js
@@ -1,14 +1,24 @@
+import * as joinActions from 'server/actions/join';
+import * as nicknameActions from 'server/actions/nickname';
 import * as serverActions from 'server/actions/server';
 import { inspect } from 'util';
 
 const subLoggers = {
   default:
     process.env.NODE_ENV === 'development' &&
-    (action => 'action: ' + inspect(action, { depth: 5 })),
+    (action => 'action: ' + inspect(action)),
   [serverActions.LISTENING]: action =>
     `WebSocket server listening on port ${action.meta.port}.`,
   [serverActions.CONNECTION]: action =>
-    `New connection from ${action.payload.request.socket.remoteAddress}`
+    `New connection from ${action.payload.request.socket.remoteAddress}`,
+  [serverActions.MESSAGE]: action =>
+    `Received message: ${inspect(action.payload)}`,
+  [serverActions.SEND]: action =>
+    `Sending message: ${inspect(action.payload.message)}`,
+  [nicknameActions.CHECK_AVAILABILITY]: action =>
+    `Checking whether the nickname "${action.payload}" is available.`,
+  [joinActions.REQUESTED]: action =>
+    `Considering a new join request from "${action.payload.nickname}".`
 };
 
 const logger = () => next => action => {
