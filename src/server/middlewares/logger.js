3519f46ac6b9e82006b1f2691269448316425149
Jacek Wilczyński <jacekwilczynski5@gmail.com>
2018-08-12 15:38:14 +0200

    Format logger output

--- src/server/middlewares/logger.js
+++ src/server/middlewares/logger.js
@@ -1,22 +1,35 @@
+import 'colors';
 import { inspect } from 'util';
 
 Object.prototype[inspect.custom] = function inspectOnlyIfPlain() {
   if (Object.getPrototypeOf(this) === Object.prototype) {
     return this;
   } else {
-    return this.constructor.name;
+    return this.constructor.name.gray;
   }
 };
 
 const subLoggers = {
   default:
     process.env.NODE_ENV === 'development' &&
-    (action => 'action: ' + inspect(action, { depth: null }))
+    (action => {
+      const actionCopy = Object.assign({}, action);
+      delete actionCopy.type;
+      return (
+        `${action.type}:\n`.blue.bold +
+        inspect(actionCopy, {
+          breakLength: 1,
+          colors: true,
+          compact: false,
+          depth: null
+        }).replace(/[{}]/g, ' ')
+      );
+    })
 };
 
 const logger = () => next => action => {
   const subLogger = subLoggers[action.type] || subLoggers.default;
-  if (subLogger) console.log(subLogger(action));
+  if (subLogger) console.log(subLogger(action) + '\n');
   next(action);
 };
 
