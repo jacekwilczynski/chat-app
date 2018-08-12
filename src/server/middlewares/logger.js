2e134052c1fe2e1538da3c88e5f9731dde679b6a
Jacek Wilczyński <jacekwilczynski5@gmail.com>
2018-08-12 17:01:18 +0200

    Simplify implementation

--- src/server/middlewares/logger.js
+++ src/server/middlewares/logger.js
@@ -12,36 +12,25 @@ Object.prototype[inspect.custom] = function() {
   }
 };
 
-const subLoggers = {
-  default:
-    process.env.NODE_ENV === 'development' &&
-    (action => {
-      const actionCopy = Object.assign({}, action);
-      delete actionCopy.type;
-      return (
-        `${action.type}:\n`.blue.bold +
-        inspect(actionCopy, {
-          breakLength: 1,
-          colors: true,
-          compact: false,
-          depth: null
-        })
-          .replace(/[{}]/g, ' ')
-          .replace(
-            /(\n\s*)(\[)(\s)/g,
-            (_, p1, p2, p3) => `${p1}${p2.gray}${p3}`
-          )
-          .replace(
-            /(\s)(])(,?\s)/g,
-            (_, p1, p2, p3) => `${p1}${p2.gray}${p3}`
-          )
-      );
+const makeString = action => {
+  const actionCopy = Object.assign({}, action);
+  delete actionCopy.type;
+  return (
+    `${action.type}:\n`.blue.bold +
+    inspect(actionCopy, {
+      breakLength: 1,
+      colors: true,
+      compact: false,
+      depth: null
     })
+      .replace(/[{}]/g, ' ')
+      .replace(/(\n\s*)(\[)(\s)/g, (_, p1, p2, p3) => `${p1}${p2.gray}${p3}`)
+      .replace(/(\s)(])(,?\s)/g, (_, p1, p2, p3) => `${p1}${p2.gray}${p3}`)
+  );
 };
 
 const logger = () => next => action => {
-  const subLogger = subLoggers[action.type] || subLoggers.default;
-  if (subLogger) console.log(subLogger(action) + '\n');
+  console.log(makeString(action));
   next(action);
 };
 
